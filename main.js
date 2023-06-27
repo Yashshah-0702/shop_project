282bc6be4a02860df46554d1906f869704585da5
yash shah <yash722002@gmail.com>
2023-06-27 14:20:55 +0530

    database_added

--- main.js
+++ main.js
@@ -3,7 +3,9 @@ const app = express()
 const body = require('body-parser')
 const route = require('./routes/route')
 const error = require('./controller/error')
+const db = require('./database/database')
 
+db.execute('SELECT * FROM products').then((result)=>{console.log(result)}).catch((err)=>{console.log(err)})
 app.set('view engine','ejs')
 app.set('views','views')
 
@@ -12,4 +14,4 @@ app.use(body.urlencoded({extended:false}))
 app.use(route)
 app.use(error.page)
 
-app.listen(5600)
+app.listen(8000)
